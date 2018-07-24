1d0729118e0f5a60f951a433d0baa82e8f866c04
Nagaraju Gaddam <nagaraju1.gaddam@gmail.com>
2018-07-24 19:07:53 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ import '../App.css';
     return (
     <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <h1 className="App-title">Volume Licensing - Unified User Management Service</h1>
+        <h1 className="App-title">it is heading section</h1>
       </header>
     )
   }
